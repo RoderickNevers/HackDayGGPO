cb5c3d2a01c0d268bf7a60a2db8cdbe679308cad
Yugo Brunet <39092819+YBYuune@users.noreply.github.com>
2021-07-22 19:25:53 -0400

    Moved transform.posion set whenever the GameManager updates the GameState

--- Build/GGPO/Assets/Scripts/Controllers/GGPOPlayerController.cs
+++ Build/GGPO/Assets/Scripts/Controllers/GGPOPlayerController.cs
@@ -18,10 +18,17 @@ public class GGPOPlayerController : MonoBehaviour
         m_GGPOComponent = ggpoComponent;
     }
 
+    // Called from GameController.cs. Is called anytime the GameManager's Update calls the Runner's update (ie advances the frame)
+    // Won't be called on rollbacks (I think)
+    public void OnStateChanged(Player player)
+    {
+        transform.position = player.Position;
+    }
+
     public void UpdatePlayerPosition(Player player)
     {
         //Debug.Log(player.Position);
-        transform.position = player.Position;
+        //transform.position = player.Position;
         //transform.position = Vector3.Slerp(transform.position, player.Position, 1f);
 
         //transform.position = player.Position;
@@ -35,5 +42,11 @@ public class GGPOPlayerController : MonoBehaviour
         //    transform.position = player.Position;
         //    m_CharacterController.Move(player.Velocity * SpeedModifier);
         //}
+
+        if (!m_GGPOComponent.manualFrameIncrement)
+        {
+            float SpeedModifier = SharedGame.GameManager.FRAME_LENGTH_SEC / m_GGPOComponent.currentFrameLength;
+            m_CharacterController.Move(player.Velocity * SpeedModifier);
+        }
     }
 }
