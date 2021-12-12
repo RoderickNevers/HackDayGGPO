1ffcba4981c6600358d9d8824eb37abf0757488a
Roderick Nevers <roderick.nevers@gmail.com>
2021-12-12 11:22:17 -0500

    players can't leave the screen

--- Build/GGPO/Assets/Scripts/StateMachines/Character/States/RetreatingState.cs
+++ Build/GGPO/Assets/Scripts/StateMachines/Character/States/RetreatingState.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Stateless;
 using System;
-//using UnityEngine.InputSystem;
 
 public class RetreatingState : CharacterStateBlock
 {
@@ -13,7 +12,7 @@ public class RetreatingState : CharacterStateBlock
     {
     }
 
-    public Player UpdatePlayer(Player player, long input)
+    public override Player UpdatePlayer(Player player, long input)
     {
         float velocity = 0;
 
@@ -58,43 +57,23 @@ public class RetreatingState : CharacterStateBlock
             }
         }
 
+        // screen bounds left
+        if (player.Position.x < -14)
+        {
+            player.Position.x = -14; //-13.9f;
+            return player;
+        }
+
+        // screen bounds right
+        if (player.Position.x > 14)
+        {
+            player.Position.x = 14; //13.9f;
+            return player;
+        }
+
         player.Velocity.Set(velocity, 0, 0);
         player.Velocity = PlayerConstants.MOVE_SPEED * Time.fixedDeltaTime * player.Velocity;
 
         return player;
     }
-
-    //private void HandleInputCommand(object sender, InputCommandArgs e)
-    //{
-    //    //Debug.Log($"Retreating Attack: {e.Attack}");
-    //    string name = e.Move != null ? e.Move.AttackData.Name : e.AttackName;
-    //    stateMachine.FireAttack(CharacterStateTrigger.TriggerAttackGround, $"{ProjectConstants.MovelistStateKeys.STANDING}-{name}", e.Move);
-    //}
-
-    //private void HandleMovement()
-    //{
-    //    switch (characterController.InputController.CurrentInputDirection)
-    //    {
-    //        case InputCommandElement.Neutral:
-    //            stateMachine.Fire(CharacterStateTrigger.TriggerStanding);
-    //            break;
-    //        case InputCommandElement.UpForward:
-    //            stateMachine.Fire(CharacterStateTrigger.TriggerJumpTowards);
-    //            break;
-    //        case InputCommandElement.DownForward:
-    //        case InputCommandElement.DownBack:
-    //        case InputCommandElement.Down:
-    //            stateMachine.Fire(CharacterStateTrigger.TriggerCrouching);
-    //            break;
-    //        case InputCommandElement.Forward:
-    //            stateMachine.Fire(CharacterStateTrigger.TriggerAdvancing);
-    //            break;
-    //        case InputCommandElement.UpBack:
-    //            stateMachine.Fire(CharacterStateTrigger.TriggerJumpBack);
-    //            break;
-    //        case InputCommandElement.Up:
-    //            stateMachine.Fire(CharacterStateTrigger.TriggerJumpUp);
-    //            break;
-    //    }
-    //}
 }
