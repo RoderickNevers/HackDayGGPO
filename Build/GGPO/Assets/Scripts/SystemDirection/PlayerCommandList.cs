bace3a4e889d446b7b59d34cba5d0bdbbbfd72f5
Roderick Nevers <rodericknevers@hotmail.com>
2021-12-28 02:11:16 -0500

    Organized the command list

--- Build/GGPO/Assets/Scripts/SystemDirection/PlayerCommandList.cs
+++ Build/GGPO/Assets/Scripts/SystemDirection/PlayerCommandList.cs
@@ -5,16 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Command List", menuName = "SystemDirection/CommandList", order = 1)]
 public class PlayerCommandList : ScriptableObject
 {
-    [SerializeField] public InputCommand Standing_Attack_A;
-    [SerializeField] public InputCommand Standing_Attack_B;
-    [SerializeField] public InputCommand Standing_Attack_C;
-    [SerializeField] public InputCommand Standing_Attack_D;
-    [SerializeField] public InputCommand Standing_Attack_E;
-    [SerializeField] public InputCommand Standing_Attack_F;
-}
+    [Header("Standing")]
+    [SerializeField] public List<InputCommand> StandingAttacks;
 
-public class AttackCommand
-{
-    [SerializeField] public string Name;
-    [SerializeField] public InputCommand Command;
+    [Header("Crouching")]
+    [SerializeField] public List<InputCommand> CrouchingAttacks;
+
+    [Header("Jumping")]
+    [SerializeField] public List<InputCommand> JumpingAttacks;
+
 }
